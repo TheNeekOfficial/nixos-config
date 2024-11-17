faf0e28b4da9ffcbabfed02bce3b39af5f465250
Dylan Leslie <dylan_leslie05@yahoo.com.au>
2024-11-17 22:07:16 +1100

    Should make nvim plugins work

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,7 +68,7 @@
   programs.neovim = 
 	let
           toLua = str: "lua << EOF\n${str}\nEOF\n";
-          toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n"
+          toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
         in
         {
           enable = true;
