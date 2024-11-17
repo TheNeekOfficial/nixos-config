f25f40450e60e2a6125c7ea2a8492d54585e4541
Dylan Leslie <dylan_leslie05@yahoo.com.au>
2024-11-17 18:30:35 +1100

    First Commit

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+ description = "A basic NixOS flake";
+
+ inputs = {
+    # NixOS official package source, using the nixos-unstable branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # NixOS Official source, setting stable version 24.05 as backup for package rollback
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    # Home Manager used for managing User configs
+    home-manager = {
+    	url = "github:nix-community/home-manager/master";
+	
+	# Follows keyword is used for inheritance
+	# Inputs.pkgs is kept consistent to version of home manager
+	# to avoid problem caused by different version of nixpkgs
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+ outputs = inputs@{ 
+    self, 
+    nixpkgs, 
+    home-manager, 
+    nixpkgs-stable, 
+    ... 
+ }: {
+    nixosConfigurations = {
+    nixed = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      specialArgs = { 
+        inherit inputs; 
+	# To use packages from nixpkgs-stable,
+        # we configure some parameters for it first
+        pkgs-stable = import nixpkgs-stable {
+          # Refer to the `system` parameter from
+          # the outer scope recursively
+          inherit system;
+          # To use Chrome, we need to allow the
+          # installation of non-free software.
+          config.allowUnfree = true;
+          };	
+      };
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+	# Make HM as module of nixos so deployments automatic
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.dylanleslie = import ./home.nix;
+
+	  # Optionally use home-manager.extraSpecialArgs to pass arguments to home.nix
+	}
+      ];
+      };
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,108 @@
+{
+  config, 
+  pkgs, 
+  pkgs-stable,
+  ... 
+  }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "dylanleslie";
+  home.homeDirectory = "/home/dylanleslie";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  home.packages = with pkgs; [
+  	
+	# terminal screens
+	neofetch
+	cowsay
+	cmatrix
+	cbonsai
+
+	# archives
+	zip
+	unzip
+
+	# misc
+	file
+	which
+	tree
+	eza
+
+	# productivity
+	btop # replaces htop/nmon
+
+	# For laptop
+	# https://nixos.wiki/wiki/Laptop 
+
+	# For stable versions
+	# pkgs-stable.a etc.
+  ];
+
+  programs.git = {
+  	enable = true;
+	userName = "TheNeekOfficial";
+	userEmail = "dylan_leslie05@yahoo.com.au";
+
+	# TO use stable version
+	# package = pkgs-stable.git;
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  # programs.alacritty = {
+  #   enable = true;
+  #   # custom settings
+  #   settings = {
+  #     env.TERM = "xterm-256color";
+  #     font = {
+  #       size = 12;
+  #       draw_bold_text_with_bright_colors = true;
+  #     };
+  #     scrolling.multiplier = 5;
+  #     selection.save_to_clipboard = true;
+  #   };
+  # };
+
+  # programs.bash = {
+  #   enable = true;
+  #   enableCompletion = true;
+  #   # TODO add your custom bashrc here
+  #   bashrcExtra = ''
+  #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+  #   '';
+
+    # set some aliases, feel free to add more or remove some
+    # shellAliases = {
+    #   k = "kubectl";
+    #   urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+    #   urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+    # };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
