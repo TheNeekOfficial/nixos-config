b5791d357dd82bda484cc5e8f92d5b0f2b84f5f9
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-09 22:47:21 +1100

    Updated tmux plugins and status line

--- home-manager/modules/packages/tmux.nix
+++ home-manager/modules/packages/tmux.nix
@@ -11,8 +11,96 @@
 		prefix = "C-a";
 
     # adds specific tmux plugins
+    # NOTE: if using set -ag make sure the -g for the same option is above it
     plugins = with pkgs.tmuxPlugins; [
-      vim-tmux-navigator
+      # NOTE: set theme at first
+
+      {
+        plugin = catppuccin; 
+        extraConfig = ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+
+          # Sets statuses to catppuccin specific ones
+          # set -g status-right "#{E:@catppuccin_status_application}"
+          # set -agF status-right "#{E:@catppuccin_status_cpu}"
+          # set -ag status-right "#{E:@catppuccin_status_session}"
+          # set -ag status-right "#{E:@catppuccin_status_uptime}"
+          # set -agF status-right "#{E:@catppuccin_status_battery}"
+          '';
+      }
+
+      # NOTE: For Catppuccin 
+      # {
+      #   plugin = cpu;
+      #   extraConfig = ''
+      #     set -agF status-right "#{E:@catppuccin_status_cpu}"
+      #     set -ag status-right "#{E:@catppuccin_status_session}"
+      #     set -ag status-right "#{E:@catppuccin_status_uptime}"
+      #     '';
+      # }
+
+      # NOTE: For Catppuccin theme
+
+      # {
+      #   plugin = battery;
+      #   extraConfig = ''
+      #     set -agF status-right "#{E:@catppuccin_status_battery}"
+      #     '';
+      # }
+
+      # NOTE: Tokyo Night Theme
+      # {
+      #   plugin = tokyo-night-tmux; 
+      #   extraConfig = ''
+      #     set -g @tokyo_night_tmux_theme night
+      #     set -g @tokyo_night_tmux_transparent 1
+      #     '';
+      # }
+
+      {
+        plugin = vim-tmux-navigator;
+      }
+
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+          '';
+      }
+
+      {
+        plugin = sysstat;
+        extraConfig = ''
+          set -g status-right ""
+          set -g status-right "#{sysstat_cpu} | #{sysstat_mem} | #{sysstat_swap} |"
+          '';
+      }
+
+      {
+        # NOTE: For default status line
+
+        plugin = battery;
+        extraConfig = ''
+         set -ag status-right '#{battery_status_bg} Batt: #{battery_icon} #{battery_percentage} #{battery_remain}'
+          set -g status-right-length '150'
+          '';
+      }
+
     ];
 
     # Sets other config options necessary
@@ -26,6 +114,9 @@
 			bind -n M-K select-pane -U
 			bind -n M-J select-pane -D
 			bind -n M-L select-pane -R
+			
+			# Adds current time to status bar
+			set -ag status-right ' | %a %h-%d %H:%M'
      '';
   };
 }
