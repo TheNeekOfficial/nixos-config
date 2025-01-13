4597c04293b328808444d1989ef168a7ee4d9231
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-13 18:53:38 +1100

    Removed tmuxinator config from tmux.nix

--- home-manager/modules/packages/tmux.nix
+++ home-manager/modules/packages/tmux.nix
@@ -8,7 +8,6 @@
     keyMode = "vi";
     # Remaps prefix from 'C-b' to 'C-a'
     prefix = "C-a";
-    tmuxinator.enable = true;
 
     # adds specific tmux plugins
     # NOTE: if using set -ag make sure the -g for the same option is above it
@@ -133,57 +132,6 @@
     '';
   };
 
-  # NOTE: Sets mux alias for tmuxinator
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      mux = "tmuxinator";
-    };
-  };
-
-  home.file."config/tmuxinator/gen.yml" = {
-    text = ''
-      # /home/dylan/.config/tmuxinator/gen.yml
-
-      name: gen
-      root: ~/
-
-      # Specifies (by name or index) which window will be selected on project startup. If not set, the first window is used.
-      startup_window: editor
-
-      windows:
-        - editor:
-            # Synchronize all panes of this window, can be enabled before or after the pane commands run.
-            # 'before' represents legacy functionality and will be deprecated in a future release, in favour of 'after'
-            # synchronize: after
-            panes:
-              - nvim
-        - bash:
-          panes:
-            - bash
-    '';
-  };
-
-  home.file."config/tmuxinator/nix.yml" = {
-    text = ''
-      # /home/dylan/.config/tmuxinator/nix.yml
-
-      name: nix
-      root: ~/nix/nixos-config/
-
-      # Specifies (by name or index) which window will be selected on project startup. If not set, the first window is used.
-      startup_window: editor
-
-      windows:
-        - editor:
-            # Synchronize all panes of this window, can be enabled before or after the pane commands run.
-            # 'before' represents legacy functionality and will be deprecated in a future release, in favour of 'after'
-            # synchronize: after
-            panes:
-              - nvim
-        - nh:
-          panes:
-            - bash
-    '';
-  };
+  # Imports tmuxinator setup
+  imports = [./tmuxinator.nix];
 }
